19d741e804e31bd26782acf0578e2220bfaf0bf5
EslamBanna <solombana2000@gmail.com>
2022-03-21 17:41:23 +0200

    delete finished trip request services

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Nexmo\Laravel\Facade\Nexmo;
 use Carbon;
+use Illuminate\Support\Facades\DB;
 // use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
@@ -21,6 +22,7 @@ class Controller extends BaseController
     public function getAllTrips(Request $request)
     {
         try {
+            DB::beginTransaction();
             $trips = Trips::where('active', 1)
                 ->with('masafr')
                 ->with('ways')
@@ -28,8 +30,27 @@ class Controller extends BaseController
                 ->where('end_date', '>', Carbon\Carbon::now())
                 ->orWhere('end_date', null)
                 ->paginate($request->paginateCount);
+            $ended_trips = Trips::where('end_date', '<', Carbon\Carbon::now())
+                ->get();
+            foreach ($ended_trips as $ended_trip) {
+                $trip = Trips::find($ended_trip['id']);
+                if (!$trip) {
+                    return $this->returnError('203', 'fail');
+                }
+                // delete complains 
+                foreach ($trip->complains as $complain) {
+                    foreach ($complain->complainList as $complainListM) {
+                        $complainListM->delete();
+                    }
+                    $complain->delete();
+                }
+                $trip->messageRooms()->delete();
+                $trip->delete();
+            }
+            DB::commit();
             return $this->returnData('data', $trips);
         } catch (\Exception $e) {
+            DB::rollback();
             return $this->returnError('201', 'fail');
         }
     }
@@ -39,10 +60,10 @@ class Controller extends BaseController
         try {
             $trips = null;
             if ($request->has('saudi') && $request->saudi == 1) {
-                $trips = RequestService::whereHas('user' , function($q){
-                    $q->where('nationality', 'like','%'.'سعودي'.'%');
+                $trips = RequestService::whereHas('user', function ($q) {
+                    $q->where('nationality', 'like', '%' . 'سعودي' . '%');
                 })
-                ->with('user')
+                    ->with('user')
                     ->with('service')
                     ->where('max_day', '>', Carbon\Carbon::now())
                     ->withCount('requestTrip as negotiation')
@@ -54,6 +75,24 @@ class Controller extends BaseController
                     ->withCount('requestTrip as negotiation')
                     ->paginate($request->paginateCount);
             }
+            $ended_request_services = RequestService::where('max_day', '<', Carbon\Carbon::now())
+                ->get();
+            foreach ($ended_request_services as $ended_request_service) {
+                $request_service = RequestService::find($ended_request_service['id']);
+                if (!$request_service) {
+                    return $this->returnError('203', 'fail');
+                }
+                // delete complains 
+                foreach ($request_service->complains as $complain) {
+                    foreach ($complain->complainList as $complainListM) {
+                        $complainListM->delete();
+                    }
+                    $complain->delete();
+                }
+                $request_service->messages()->delete();
+                $request_service->delete();
+            }
+            DB::commit();
             return $this->returnData('data', $trips);
         } catch (\Exception $e) {
             return $this->returnError('201', 'fail');
